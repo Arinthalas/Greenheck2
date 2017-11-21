920daaa1a1760667cf0ad68d83eb256213a92bfe
Arinthalas <32649470+Arinthalas@users.noreply.github.com>
2017-11-20 22:06:33 -0600

    Add files via upload

--- Greenheck-master/Greenheck Project/Problem Domain/FocusComments.cs
+++ Greenheck-master/Greenheck Project/Problem Domain/FocusComments.cs
@@ -8,30 +8,33 @@ namespace Greenheck_Project.Problem_Domain
 {
     class FocusComments
     {
-        private DateTime fiscalYear;
+        private int fiscalYear;
         private int quarter;
         private int focusID;
         private int projectID;
+        private int statusID;
         private string comments;
 
-        public DateTime FiscalYear { get => fiscalYear; set => fiscalYear = value; }
+        public int FiscalYear { get => fiscalYear; set => fiscalYear = value; }
         public int Quarter { get => quarter; set => quarter = value; }
         public int FocusID { get => focusID; set => focusID = value; }
         public int ProjectID { get => projectID; set => projectID = value; }
         public string Comments { get => comments; set => comments = value; }
+        public int StatusID { get => statusID; set => statusID = value; }
 
         public FocusComments()
         {
 
         }
 
-        public FocusComments(DateTime year, int q, int focus, int proj, string comment)
+        public FocusComments(int year, int q, int focus, int stat, int proj, string comment)
         {
             FiscalYear = year;
             Quarter = q;
             FocusID = focus;
             ProjectID = proj;
             Comments = comment;
+            StatusID = stat;
         }
     }
 }
